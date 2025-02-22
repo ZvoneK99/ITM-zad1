9cbf8b03030c248ecf467edc92c2cf23b48cb273
Zvone <zvone.kozull1999@gmail.com>
2025-02-22 15:45:46 +0100

    Update README and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
-function App() {
+/**1.domaci 
+ * novi react projekt
+ * napraviti dark mode, ako je trenuto vrijeme od 20h - 07h da je background "dark blue"
+ */
+/*  PROVJERA RADI LI SVE pomocu custom time
+const trialTime = new Date().setHours(15, 0, 0, 0);
+const formattedTime = new Date(trialTime).toLocaleTimeString();
+const hours = new Date(trialTime).getHours();
+*/
+const currentTime = new Date().toLocaleTimeString();
+const currentTimeHours = new Date(currentTime).getHours();
+
+console.log(currentTime);
+
+const darkMode = "darkblue";
+const lightMode = "white";
+
+let mode;
+if(currentTimeHours >= 20 || currentTimeHours <= 7) {
+  mode = darkMode;
+}
+else {
+  mode = lightMode;
+}
+
+function homework() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timeDiv" style={{ background: mode}}>Trenutno vrijeme je: { currentTime }
+    <div ></div>
     </div>
   );
 }
 
-export default App;
+export default homework;
